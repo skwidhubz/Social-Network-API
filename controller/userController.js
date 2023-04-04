c396ba04e067f5868b210991af3f04d80c94570f
skwidhubz <tommaxwellparker@gmail.com>
2023-04-04 10:17:41 +1000

    yeebs

--- controller/userController.js
+++ controller/userController.js
@@ -1,3 +1,20 @@
 // ObjectId() method for converting studentId string into an ObjectId for querying database
 const { ObjectId } = require('mongoose').Types;
+const User = require('../models');
 
+
+
+module.exports = {
+    // get all users
+
+
+    // get a single user
+
+
+    // create a user
+    
+
+    // delete a user
+
+
+}
