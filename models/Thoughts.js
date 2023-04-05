61b42cfc6d85f98c430bf53587d7a6817697c1c8
skwidhubz <tommaxwellparker@gmail.com>
2023-04-05 16:53:40 +1000

    :skull: this sux mk2 :bug:

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -1,5 +1,5 @@
 const { Schema, model } = require('mongoose');
-const Reactions = require('./Reactions');
+const reactionSchema = require('./Reactions');
 
 const thoughtsSchema = new Schema(
     {
@@ -18,7 +18,7 @@ const thoughtsSchema = new Schema(
             type: String,
             required: true,
         },
-        reactions: [Reactions]
+        reactions: [reactionSchema]
     },
     {   // Schema Settings
         toJSON: {
